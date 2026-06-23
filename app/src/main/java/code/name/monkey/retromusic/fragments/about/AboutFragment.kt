4888a9af03e9f4dcecdadf4fcb5a57455026c8ed
eymade <eymade@users.noreply.github.com>
2026-06-23 04:48:43 +0000

    fix: remove loadContributors, use static credit info

--- app/src/main/java/code/name/monkey/retromusic/fragments/about/AboutFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/about/AboutFragment.kt
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) 2020 Hemanth Savarla.
- *
- * Licensed under the GNU General Public License v3
- *
- * This is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- */
 package code.name.monkey.retromusic.fragments.about
 
 import android.content.pm.PackageManager
@@ -19,31 +5,23 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
 import code.name.monkey.retromusic.App
 import code.name.monkey.retromusic.Constants
 import code.name.monkey.retromusic.R
-import code.name.monkey.retromusic.adapter.ContributorAdapter
 import code.name.monkey.retromusic.databinding.FragmentAboutBinding
 import code.name.monkey.retromusic.extensions.openUrl
-import code.name.monkey.retromusic.fragments.LibraryViewModel
 import code.name.monkey.retromusic.util.NavigationUtil
 import dev.chrisbanes.insetter.applyInsetter
-import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class AboutFragment : Fragment(R.layout.fragment_about), View.OnClickListener {
     private var _binding: FragmentAboutBinding? = null
     private val binding get() = _binding!!
-    private val libraryViewModel by activityViewModel<LibraryViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAboutBinding.bind(view)
         binding.aboutContent.cardOther.version.setSummary(getAppVersion())
         setUpView()
-        loadContributors()
-
         binding.aboutContent.root.applyInsetter {
             type(navigationBars = true) {
                 padding(vertical = true)
@@ -59,12 +37,10 @@ class AboutFragment : Fragment(R.layout.fragment_about), View.OnClickListener {
         binding.aboutContent.cardRetroInfo.appShare.setOnClickListener(this)
         binding.aboutContent.cardRetroInfo.donateLink.setOnClickListener(this)
         binding.aboutContent.cardRetroInfo.bugReportLink.setOnClickListener(this)
-
         binding.aboutContent.cardSocial.telegramLink.setOnClickListener(this)
         binding.aboutContent.cardSocial.twitterLink.setOnClickListener(this)
         binding.aboutContent.cardSocial.pinterestLink.setOnClickListener(this)
         binding.aboutContent.cardSocial.websiteLink.setOnClickListener(this)
-
         binding.aboutContent.cardOther.changelog.setOnClickListener(this)
         binding.aboutContent.cardOther.openSource.setOnClickListener(this)
         // 彩蛋：点击版本号弹出"王馨玉我爱你"
@@ -112,18 +88,6 @@ class AboutFragment : Fragment(R.layout.fragment_about), View.OnClickListener {
             .setChooserTitle(R.string.share_app)
             .setText(String.format(getString(R.string.app_share), requireActivity().packageName))
             .startChooser()
-    }
-
-    private fun loadContributors() {
-        val contributorAdapter = ContributorAdapter(emptyList())
-        binding.aboutContent.cardCredit.recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            itemAnimator = DefaultItemAnimator()
-            adapter = contributorAdapter
-        }
-        libraryViewModel.fetchContributors().observe(viewLifecycleOwner) { contributors ->
-            contributorAdapter.swapData(contributors)
-        }
     }
 
     override fun onDestroyView() {
